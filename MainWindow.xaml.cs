2c7e9239284d80abd396235ec81d4e7f1107583e
Roman <github@reejk.xyz>
2023-07-14 01:19:58 +0300

    project to reproduce wpf bug

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace WpfBitmapCacheRenderThreadFail
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static readonly int[] Items5000 = Enumerable.Range(0, 5000).ToArray();
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
